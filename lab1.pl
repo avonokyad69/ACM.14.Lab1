111fb0e86db2dc38fe4254568cc526a7c043823a
dl <dl@bugtraq.ru>
2014-09-11 19:43:21 +0400

    menu

--- lab1.pl
+++ lab1.pl
@@ -1,5 +1,48 @@
 #!/usr/bin/perl
 
+use strict;
 use st01::st01;
 
-ST01::st01();
+my @MODULES = 
+(
+	\&ST01::st01,
+	\&ST01::st01,
+	\&ST01::st01,
+	\&ST01::st01,
+);
+
+my @NAMES = 
+(
+	"Student 01",
+	"Student 02",
+	"Student 03",
+	"Student 04",
+);
+
+sub menu
+{
+	my $i = 0;
+	print "\n------------------------------\n";
+	foreach my $s(@NAMES)
+	{
+		$i++;
+		print "$i. $s\n";
+	}
+	print "------------------------------\n";
+	my $ch = <STDIN>;
+	return ($ch-1);
+}
+
+while(1)
+{
+	my $ch = menu();
+	if(defined $MODULES[$ch])
+	{
+		print $NAMES[$ch]." launching...\n\n";
+		$MODULES[$ch]->();
+	}
+	else
+	{
+		exit();
+	}
+}
--- st01/st01.pm
+++ st01/st01.pm
@@ -1,6 +1,9 @@
 package ST01;
 
-
+sub st01
+{
+	print "st01\n";
+}
 
 
 return 1;
