fd3de8f13a6ac76759f72c8ffa1b7d30a3ee9846
Alexander Mansurov <aleksandrmansurov@MacBook-Aleksandr-Mansurov.local>
2014-09-13 20:51:49 +0400

    Test Commit
    Проверяю, правильно ли я понял, как работают коммиты на гитхабе.

--- lab1.pl
+++ lab1.pl
@@ -3,17 +3,22 @@
 use strict;
 use st01::st01;
 use st02::st02;
+use st03::st03;
+#так надо? что-то пока не очень понятно
+#просто проверяю
 
 my @MODULES = 
 (
 	\&ST01::st01,
 	\&ST02::st02,
+	\&ST03::st03,
 );
 
 my @NAMES = 
 (
 	"Student 01",
 	"Student 02",
+	"Student 03",
 );
 
 sub menu
