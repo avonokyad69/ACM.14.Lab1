9aa2ea8790b137a09fb8b618322ef1dadbc6cf90
dl <dl@bugtraq.ru>
2014-10-18 13:36:13 +0400

    info

--- lab1.pl
+++ lab1.pl
@@ -7,10 +7,6 @@ use st08::st08;
 use st15::st15;
 use st22::st22;
 
-# Если для своей работы создаете отдельный форк, не забывайте его синхронизировать перед отправкой:
-# https://help.github.com/articles/syncing-a-fork/
-#
-
 my @MODULES = 
 (
 	\&ST01::st01,
